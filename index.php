0ad041e27fb83122bda8e3af49d1a121a9d03c57
fatmaaldobai3-png <fatmaaldobai3@gmail.com>
2026-08-31 15:30:41 +0300

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>جمعية السعدي الخيرية التنموية - عدن</title>
-    <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="/css/style.css?v=2">
 </head>
 <body>
     <!-- Navbar -->
